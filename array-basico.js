8db8a9303dbde6c00fa0c0460a5dd09e8ceae92e
mmgranda <mariadelmgranda15@gmail.com>
2026-04-29 16:03:52 -0500

    Arrays for y for en JavaScript semana 3 dia 4

--- array-basico.js
+++ array-basico.js
@@ -1,6 +1,6 @@
-const comunidades = ["Barrio La Paz", "Comunidad Escolar", "Junta Juvenil"]
+const comunidades1 = ["Barrio La Paz", "Comunidad Escolar", "Junta Juvenil"]
 
 console.log("Primer ejemplo de array")
 console.log("Un array permite guardar varios datos en una sola variable.")
 console.log("Comunidades registradas:")
-console.log(comunidades)
+console.log(comunidades1)
