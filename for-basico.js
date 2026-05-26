b4febc40bc7b96c9ff4dd2836af70e1b514b4e9f
JoshuaKatze <joshuafernandezestudio@gmail.com>
2026-05-26 10:09:55 -0500

    Arrays for y for of en JavaScript semana 3 dia 4

--- for-basico.js
+++ for-basico.js
@@ -1,4 +1,4 @@
-const comunidades = ["Barrio La Paz", "Comunidad Escolar", "Junta Juvenil"]
+const comunidades = ["Barrio La Paz", "Comunidad Escolar", "Junta Juvenil", "Comunidad de la Salud", "Comunidad de la Cultura", "Comunidad Deportiva"]
 
 console.log("Recorrido de un array con for tradicional")
 
